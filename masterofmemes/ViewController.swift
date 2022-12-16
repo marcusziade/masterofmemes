1dcb54788289e88d1267152e14bd09b468f2bea1
Marcus Ziadé <marcus.ziade@ouraring.com>
2022-12-16 17:20:12 +0200

    Initial Commit

---
+++ masterofmemes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  masterofmemes
+//
+//  Created by Marcus Ziadé on 16.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
